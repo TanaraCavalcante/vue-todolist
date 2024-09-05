e9d715b23e023a59fd0a9f7e2c08108bd9a295cc
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-09-05 16:09:40 -0400

    true or false?

--- JS/script.js
+++ JS/script.js
@@ -3,10 +3,46 @@ const {createApp} = Vue;
 createApp ({
     data(){
         return {
-
+         list:[
+            {
+                text: 'Fare colazione',
+                done: 'false'
+            },
+            {
+                text: 'Andare al dentista',
+                done: 'false'
+            },
+            {
+                text: 'Andare al lavoro',
+                done: 'false'
+            },
+            {
+                text: 'Avviare il computer',
+                done: 'false'
+            },
+            {
+                text: 'Allenarsi',
+                done: 'false'
+            },
+            {
+                text: 'Cena con la famiglia',
+                done: 'false'
+            },
+         ]
         }
     },
     methods:{
+        isDone() {
+            if(this.list.done == false){
+               this.list.done == true;
+               return;
+            }else {
+                this.list.done == false;
+            }
+        }
 
     },
 }).mount('#app')
+
+//Criar um booleano pra definir se "done" é verdadeiro ou falso;
+//Conectar esse booleano com o "X" botao ao lado de cada item da lista.
